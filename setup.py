ffb1870c4f258dcb84581f51452bfc9411eb0055
Smowens <sowens28@tjs.org>
2025-04-24 00:22:18 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author='Sam Owens',
     author_email='sowens28@tjs.org',
     packages=['tools'],
-    package_dir={'tools': 'Users/sowen/Python Files'},
+    package_dir={'tools': 'Users/sowens/Python Files'},
     install_requires=['setuptools', 'wheel']
 )
