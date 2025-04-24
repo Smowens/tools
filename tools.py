4381cc678e823ed1fe206b0ede7e7a6614787b85
Smowens <sowens28@tjs.org>
2025-04-23 21:39:45 -0500

    Delete tools.py

--- tools.py
+++
@@ -1,8 +0,0 @@
-Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
-def Percent_Calc(data):
-    total = sum(data)
-    result = {}
-    for X in data:
-    result[X] = (X / total) * 100
-	return result
