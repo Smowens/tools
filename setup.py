93bf7cacd2d7fd2defc8d617646ddb3a46338ed3
Smowens <sowens28@tjs.org>
2025-04-24 00:14:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
+
 setup(
-name='tools',
-version='1.0',
-description="Sam's Tool Module",
-author='Sam Owens',
-author_email='sowens28@tjs.org',
-packages=['tools'],
-package_dir{'tools':'Users/sowen/Python Files'},
-install_requires=['setuptools','wheel']
+    name='tools',
+    version='1.0',
+    description="Sam's Tool Module",
+    author='Sam Owens',
+    author_email='sowens28@tjs.org',
+    packages=['tools'],
+    package_dir={'tools': 'Users/sowen/Python Files'},
+    install_requires=['setuptools', 'wheel']
 )
