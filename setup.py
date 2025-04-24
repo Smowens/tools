4d54eaef482ecb8437ca65e7005148f770bf9515
Smowens <sowens28@tjs.org>
2025-04-23 23:28:50 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ description="Sam's Tool Module",
 author='Sam Owens',
 author_email='sowens28@tjs.org',
 packages=['tools'],
+package_dir{'':'https://github.com/Smowens/tools/blob/main/tools.py'}
 install_requires=['setuptools','wheel']
 )
