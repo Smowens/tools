bd5e9ae1f8eca6d0f31b52ec0b9aad06864fde96
Smowens <sowens28@tjs.org>
2025-04-25 16:03:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='tools',
-    version='1.2',
+    version='1.2.1',
     description="Sam's Tool Module",
     author='Sam Owens',
     author_email='sowens28@tjs.org',
