9a80293c67f078a0911c51f7b7c4375f670af7c3
Smowens <sowens28@tjs.org>
2025-04-25 10:35:59 -0500

    Update tools.py

--- tools.py
+++ tools.py
@@ -5,12 +5,12 @@ def Percent_Calc(data):
 	result_lastkey=[]
 	result_unfinished={}
 	for X in data:
-    		result_formatted[X] = (X / total) * 100
-	result_lastkey=list(result_unformatted.key())[-1]
+		result_unformatted[X] = (X / total) * 100
 	for number,label in result_unformatted.items():
 		result_unfinished[number]='{:.2f}'.format(label)
 	for number,label in result_unfinished.items():
 		result[label]='{:,}'.format(number)
-	value={value:key for key,value in result_unfinished.items()}
+	result={value:key for key,value in result.items()}
+	result_lastkey=list(result.keys())[-1]
 	del result[result_lastkey]
 	return result
