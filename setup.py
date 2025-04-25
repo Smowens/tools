d811a4993823c010bf94f81a11894f636bb23df8
Smowens <sowens28@tjs.org>
2025-04-25 13:16:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     description="Sam's Tool Module",
     author='Sam Owens',
     author_email='sowens28@tjs.org',
-    packages=['tools'],
     py_modules=['tools','setup'],
     install_requires=['setuptools', 'wheel']
 )
