30761765b38c579998fba9230d4a987f96b0a25d
Smowens <sowens28@tjs.org>
2025-04-25 13:31:44 -0500

    Update and rename tools.py to Percent_Calc.py

---
+++ Percent_Calc.py
@@ -0,0 +1,14 @@
+total = sum(data)
+result = {}
+result_unformatted={}
+result_lastkey=[]
+result_unfinished={}
+for X in data:
+	result_unformatted[X] = (X / total) * 100
+for number,label in result_unformatted.items():
+	result_unfinished[number]='{:.2f}'.format(label)
+for number,label in result_unfinished.items():
+	result[label]='{:,}'.format(number)
+result={value:key for key,value in result.items()}
+result_lastkey=list(result.keys())[-1]
+del result[result_lastkey]
